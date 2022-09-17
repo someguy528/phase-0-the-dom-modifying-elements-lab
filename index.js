abd6ca06c3736f8cbe687de6963ca9b085cde816
Brandon Castro <castroyou999@gmail.com>
2022-09-16 21:40:59 -0400

    commit tree 70a7557eb2e5b0d7f1b98a5655934babedfd6015

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+const newHeader = document.createElement("h1")
+newHeader.id= "victory";
+newHeader.textContent = 'Brandon is the champion';
+document.body.append(newHeader);
+
+
